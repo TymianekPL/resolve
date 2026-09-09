f2cec72aa243457bd36c150f5d7ff0f1fca415f6
Tymi <tymi@tymi.org>
2026-09-09 14:21:23 +0200

    Add mariadb to the backend (#9)
    * Add mariadb as a dependency to the backend
    
    * Add dotenv as a dependency & example env file
    
    Signed-off-by: TymianekPL <tymi@tymi.org>
    
    * Add a database connection
    
    * eslint
    
    Signed-off-by: TymianekPL <tymi@tymi.org>
    
    ---------
    
    Signed-off-by: TymianekPL <tymi@tymi.org>

---
+++ backend/src/db.ts
@@ -0,0 +1,29 @@
+import mariadb, { type Pool, type PoolConnection } from "mariadb";
+
+export let pool: Pool;
+
+export const checkDb = async () => {
+     if (!process.env["DB_USER"] || !process.env["DB_NAME"]) {
+          throw "Environment not defined.";
+     }
+     if (!process.env["DB_PASSWORD"]) {
+          console.warn("Empty db password");
+     }
+     pool = mariadb.createPool({
+          host: process.env["DB_HOST"] ?? "localhost",
+          port: parseInt(process.env["DB_PORT"] ?? "3306"),
+          user: process.env["DB_USER"]!,
+          password: process.env["DB_PASSWORD"] ?? "",
+          database: process.env["DB_NAME"]!
+     });
+
+     let connection: PoolConnection | null = null;
+     try {
+          connection = await pool.getConnection();
+          await connection.query("SELECT 1;");
+          console.log("Connected to the database");
+     }
+     finally {
+          if (connection) connection.release();
+     }
+};
